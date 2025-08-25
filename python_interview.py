577e5131e17c57d4a9d204fd5f78e6176906735b
Prakhar Gupta <58927877+prakharcr704@users.noreply.github.com>
2025-08-25 23:27:49 +0530

    Update python_interview.py

--- python_interview.py
+++ python_interview.py
@@ -9,3 +9,12 @@ common = set(s1) & set(s2)
 # return a sorted list of common alphabets.
 print(sorted(list(common)))
 
+# Another way using for loop and iterate over the strings.
+common = []
+
+for ch in s1.lower():
+    if ch.isalpha() and ch in s2.lower() and ch not in common:
+        common.append(ch)
+print(common)
+
+# ---------------------------------------------------------------------------------------------------------------
