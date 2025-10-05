d6fc345d976ec9485728485a59e6fe8c4f059405
Prakhar Gupta <58927877+prakharcr704@users.noreply.github.com>
2025-10-06 00:02:54 +0530

    Update sql_inteview.sql

--- sql_inteview.sql
+++ sql_inteview.sql
@@ -6,3 +6,18 @@ select emp_id, count(1) from emp group by emp_id having count(1)>1
 --2. How to delete duplicates?
 with cte as ( select *, row_number() over(partition by emp_id order by emp_id) as rn from emp1)
 delete from cte where rn > 1
+
+--3 Difference between union and union all
+  -- union all 
+    -- will result in having all the row from both table.
+    -- eg table 1 (9 rows) + table 2 (8 rows) = 17 rows ( will also have duplicate row from table 1 )
+  -- union
+    -- will remove duplicates 
+
+select manager_id from emp
+union
+select manager_id from emp1
+--above query will give only unique manager ids.
+
+
+
