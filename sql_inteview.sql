1c11d8f7092baae831b84d3ef0b79716fecd0676
Prakhar Gupta <58927877+prakharcr704@users.noreply.github.com>
2025-10-05 23:55:55 +0530

    Update sql_inteview.sql

--- sql_inteview.sql
+++ sql_inteview.sql
@@ -2,3 +2,7 @@
 
 --1. How to find duplicates in a given table?
 select emp_id, count(1) from emp group by emp_id having count(1)>1
+
+--2. How to delete duplicates?
+with cte as ( select *, row_number() over(partition by emp_id order by emp_id) as rn from emp1)
+delete from cte where rn > 1
